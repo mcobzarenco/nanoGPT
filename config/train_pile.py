535fdfbdb00294b8729c85ab4e80a6f17d6eaf76
Marius Cobzarenco <marius@reinfer.io>
2024-01-14 04:22:22 +0000

    wip

--- config/train_pile.py
+++ config/train_pile.py
@@ -7,19 +7,19 @@ wandb_project = 'latte'
 wandb_run_name=''
 
 # model
-n_layer = 16
-n_head = 16
-n_embd = 1024
+n_layer = 12
+n_head = 12
+n_embd = 768
 dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
 bias = False # do we use bias inside LayerNorm and Linear layers?
 
-# these make the total batch size be ~1M
-# 32 batch size * 2048 block size * 2 gradaccum * 8 GPUs = 1,048,576
-batch_size = 32
+# these make the total batch size be ~0.5M
+# 32 batch size * 1024 block size * 2 gradaccum * 8 GPUs = 524,288
+batch_size = 64
 block_size = 1024
-gradient_accumulation_steps = 2 * 8
+gradient_accumulation_steps = 1 * 8
 
-# this makes total number of tokens be 300B
+# this makes total number of tokens be 150B
 max_iters = 300000
 lr_decay_iters = 300000
 
